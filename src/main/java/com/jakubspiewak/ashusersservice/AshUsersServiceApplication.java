e271bc43d23de3a288b0ec5f84b4aa7ae19b740b
Jakub Śpiewak <priv@jakubspiewak.com>
2021-07-12 22:58:20 +0200

    Working dicovery server integration

--- src/main/java/com/jakubspiewak/ashusersservice/AshUsersServiceApplication.java
+++ src/main/java/com/jakubspiewak/ashusersservice/AshUsersServiceApplication.java
@@ -2,7 +2,10 @@ package com.jakubspiewak.ashusersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AshUsersServiceApplication {
 
